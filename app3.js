9fdd6fd965c96b3fad32d2f43787f880b3f0d645
bronwyndet <wayward@waywardcoffee.com>
2016-07-26 18:34:23 -0700

    Table completed! all data populating per assignment

--- app3.js
+++ app3.js
@@ -32,47 +32,27 @@ function StoreSalesProjections (storeName, minCustHour, maxCustHour, avgCookiesC
     }
   };
 
-// FUNCTION TO CREATE SALES DATA TABLE
-  this.createSalesTable = function () {
+// METHOD TO CREATE SALES TABLE DATA CELLS
+  this.createSalesDataCells = function () {
     var salesDataTable = document.getElementById('salesDataTable');
 
-    this.makeHeaderRow = function () {
-      var salesDataRow = document.createElement('tr');
+    var salesDataRow = document.createElement('tr');
+    var salesDataNameCell = document.createElement('td');
+    salesDataNameCell.textContent = this.storeName;
+    salesDataRow.appendChild(salesDataNameCell);
+
+    for (var i = 0; i < this.projCookiesPerHour.length; i++) {
       var salesDataCell = document.createElement('td');
-      salesDataCell.textContent = '';
+      salesDataCell.textContent = this.projCookiesPerHour[i];
       salesDataRow.appendChild(salesDataCell);
-      salesDataTable.appendChild(salesDataRow);
+    }
 
-      for (var i = 0; i < openHours.length; i++) {
-        var salesDataHeader = document.createElement('th');
-        salesDataHeader.textContent = openHours[i];
-        salesDataRow.appendChild(salesDataHeader);
-        salesDataTable.appendChild(salesDataRow);
-      }
-    };
+    var salesDataTotalCell = document.createElement('td');
+    salesDataTotalCell.textContent = this.totalDailyCookies;
+    salesDataRow.appendChild(salesDataTotalCell);
+
+    salesDataTable.appendChild(salesDataRow);
   };
-
-
-
-  //MAKE THIS A STAND ALONE FUNCTION OUTSIDE OF THE CONSTRUCTOR TO MAKE FOOTER ROW  --- NEEDS TO BE ADAPATED
-    this.createSalesTable = function () {
-      var salesDataTable = document.getElementById('salesDataTable');
-
-      this.makeHeaderRow = function () {
-        var salesDataRow = document.createElement('tr');
-        var salesDataEmptyHeader = document.createElement('th');
-        salesDataEmptyHeader.textContent = '';
-        salesDataRow.appendChild(salesDataEmptyHeader);
-        salesDataTable.appendChild(salesDataRow);
-
-        for (var i = 0; i < openHours.length; i++) {
-          var salesDataHeader = document.createElement('th');
-          salesDataHeader.textContent = openHours[i];
-          salesDataRow.appendChild(salesDataHeader);
-          salesDataTable.appendChild(salesDataRow);
-        }
-      };
-    };
 
 // // METHOD TO RENDER CUSTOMERS PER HOUR & COOKIES PER HOUR LISTS TO HTML
 //   this.renderLists = function() {
@@ -89,7 +69,6 @@ function StoreSalesProjections (storeName, minCustHour, maxCustHour, avgCookiesC
 
   this.calcRandomCustHour();
   this.calcCookiesPerHour();
-  this.createSalesTable();
   // this.renderLists();
 };
 
@@ -98,6 +77,14 @@ var seatacAirport = new StoreSalesProjections(storeNames[1], 3, 24, 1.2); //esli
 var seattleCenter = new StoreSalesProjections(storeNames[2], 11, 38, 3.7); //eslint-disable-line
 var capitalHill = new StoreSalesProjections(storeNames[3], 20, 38, 2.3); //eslint-disable-line
 var alki = new StoreSalesProjections(storeNames[4], 2, 16, 4.6); //eslint-disable-line
+
+makeHeaderRow();
+firstPike.createSalesDataCells();
+seatacAirport.createSalesDataCells();
+seattleCenter.createSalesDataCells();
+capitalHill.createSalesDataCells();
+alki.createSalesDataCells();
+
 
 //MAKE THIS A STAND ALONE FUNCTION OUTSIDE OF THE CONSTRUCTOR TO MAKE HEADER ROW  --- NEEDS TO BE ADAPATED
 function makeHeaderRow () {
@@ -113,10 +100,31 @@ function makeHeaderRow () {
     salesDataHeader.textContent = openHours[i];
     salesDataRow.appendChild(salesDataHeader);
   }
-  var salesDataEmptyHeader = document.createElement('th');
+  salesDataEmptyHeader = document.createElement('th');
   salesDataEmptyHeader.textContent = 'Daily Total';
   salesDataRow.appendChild(salesDataEmptyHeader);
+
   salesDataTable.appendChild(salesDataRow);
 };
 
-makeHeaderRow();
+// STAND-ALONE FUNCTION FOR FOOTER **still to be adapted**
+// function makeFooterRow () {
+//   var salesDataTable = document.getElementById('salesDataTable');
+//
+//   var salesDataRow = document.createElement('tr');
+//   var salesDataEmptyHeader = document.createElement('th');
+//   salesDataEmptyHeader.textContent = '';
+//   salesDataRow.appendChild(salesDataEmptyHeader);
+//
+//   for (var i = 0; i < openHours.length; i++) {
+//     var salesDataHeader = document.createElement('th');
+//     salesDataHeader.textContent = openHours[i];
+//     salesDataRow.appendChild(salesDataHeader);
+//   }
+//   var salesDataEmptyHeader = document.createElement('th');
+//   salesDataEmptyHeader.textContent = 'Daily Total';
+//   salesDataRow.appendChild(salesDataEmptyHeader);
+//   salesDataTable.appendChild(salesDataRow);
+// };
+//
+// makeFooterRow();
